45d22439c511f81c697754168d5654d3229594b8
Raviraj <ravirajaade299@gmail.com>
2025-10-29 22:47:17 +0530

    Fix: ai modle add add changes in config.py

--- realeye_backend/app/config.py
+++ realeye_backend/app/config.py
@@ -16,7 +16,7 @@ class Config:
     
     
     HUGGINGFACE_API_TOKEN = os.getenv("HUGGINGFACE_API_TOKEN")
-    HF_MODEL = os.getenv("HF_MODEL", "mrm8488/t5-base-finetuned-conversational")
+    HF_MODEL = os.getenv("HF_MODEL", "microsoft/DialoGPT-small")
 
 # Optional aliases for old references
 # ASTRA_DB_TOKEN = Config.ASTRA_DB_APPLICATION_TOKEN
